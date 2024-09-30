18b858277c5fb1422c6a019b7f3da46cb0127744
yeswanth <yswnth.2010@gmail.com>
2024-09-30 08:58:54 +0530

    expense terraform

--- 10-vpc/parameters.tf
+++ 10-vpc/parameters.tf
@@ -21,3 +21,9 @@ resource "aws_ssm_parameter" "database_subnet_ids" {
   type  = "StringList"
   value = join(",", module.vpc.database_subnet_ids)
 }
+
+resource "aws_ssm_parameter" "db_subnet_group_name" {
+  name = "/${var.project_name}/${var.environment}/db_subnet_group_name"
+  type = "String"
+  value = module.vpc.db_subnet_group_name
+}
