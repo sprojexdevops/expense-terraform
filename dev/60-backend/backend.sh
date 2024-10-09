6d3c15d99015a24e750cca28ab3bf3a2bccd31a2
yeswanth <yswnth.2010@gmail.com>
2024-10-09 16:06:41 +0530

    expense cloud terraform

--- dev/60-backend/backend.sh
+++ dev/60-backend/backend.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-#get the values from terraform
+# values are passed terraform --> shell script --> ansible
 component=$1
-environment=$2         
-#echo "Component: $component, Environment: $environment"
-#pass values to ansible playbook from shell script
+environment=$2
+echo "Component: $component, Environment: $environment"
 dnf install ansible -y
 ansible-pull -i localhost, -U https://github.com/sprojexdevops/3tier-ansible-roles-tf.git main.yaml -e component=$component -e target_environment=$environment
