329bf5680c037f0b92e5783c34bf3f4311240302
yeswanth <yswnth.2010@gmail.com>
2024-10-10 19:57:52 +0530

    expense terraform

--- dev/60-backend/backend.sh
+++ dev/60-backend/backend.sh
@@ -4,5 +4,5 @@
 component=$1
 environment=$2
 echo "Component: $component, Environment: $environment"
-sudo dnf install ansible -y
+sudo dnf -y install ansible
 ansible-pull -i localhost, -U https://github.com/sprojexdevops/3tier-ansible-roles-tf.git main.yaml -e component=$component -e target_environment=$environment
